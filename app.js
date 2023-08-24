3804785d051546ff9149d1937dd4acc7e6476b9c
Aki <ancgrouptx@gmail.com>
2023-08-24 17:53:58 -0500

    js is updated

--- app.js
+++ app.js
@@ -1,25 +1,58 @@
-$(document).ready(function(){
-    //Change box colors//
-    $("#changeBox1").click(function() {
-        $("#box1").css("background-color", "blue");
-        $("#box1").css("border-color", "orange");
+document.addEventListener("DOMContentLoaded", function() {
+    const box1 = document.getElementById("box1");
+    const box2 = document.getElementById("box2");
+    const box3 = document.getElementById("box3");
+    
+    const changeBox1Btn = document.getElementById("changeBox1");
+    const changeBox2Btn = document.getElementById("changeBox2");
+    const changeBox3Btn = document.getElementById("changeBox3");
+    const changeAllBtn = document.getElementById("changeAll");
+    
+    const hideBox1Btn = document.getElementById("hideBox1");
+    const hideBox2Btn = document.getElementById("hideBox2");
+    const hideBox3Btn = document.getElementById("hideBox3");
+    const hideAllBtn = document.getElementById("hideAll");
+    
+    const resetColorsBtn = document.getElementById("resetColors");
+    const showAllBtn = document.getElementById("showAll");
+  
+    changeBox1Btn.addEventListener("click", function() {
+      box1.style.backgroundColor = "orange";
+      box1.style.borderColor = "purple";
     });
-    $("#changeBox2").click(function() {
-        $("#box2").css("background-color", "yellow");
-        $("#box2").css("border-color", "green");
+  
+    changeBox2Btn.addEventListener("click", function() {
+      box2.style.backgroundColor = "yellow";
+      box2.style.borderColor = "green";
     });
-    $("#changeBox3").click(function() {
-        $("#box3").css("background-color", "red");
-        $("#box3").css("border-color", "purple");
+  
+    changeBox3Btn.addEventListener("click", function() {
+      box3.style.backgroundColor = "pink";
+      box3.style.borderColor = "red";
     });
-
-    //Hide boxes
-    $("#hideBox1").click(function(){
-    $("#box1").hide();
+  
+    changeAllBtn.addEventListener("click", function() {
+      box1.style.backgroundColor = "cyan";
+      box1.style.borderColor = "blue";
+      box2.style.backgroundColor = "cyan";
+      box2.style.borderColor = "blue";
+      box3.style.backgroundColor = "cyan";
+      box3.style.borderColor = "blue";
     });
-    $("#hideBox2").click(function(){
-        $("#box2").hide();
-        });
-    $("#hideBox3").click(function(){
-            $("#box3").hide();
-            });
+  
+    hideBox1Btn.addEventListener("click", function() {
+      box1.style.display = "none";
+    });
+  
+    hideBox2Btn.addEventListener("click", function() {
+      box2.style.display = "none";
+    });
+  
+    hideBox3Btn.addEventListener("click", function() {
+      box3.style.display = "none";
+    });
+  
+    hideAllBtn.addEventListener("click", function() {
+      box1.style.display = "none";
+      box2
+  
